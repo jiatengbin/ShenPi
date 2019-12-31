7eefe0010cdd339973e33b7618a73c41b5f509f0
“jiatengbin” <jtb0403@163.com>
2019-12-31 15:44:01 +0800

    这是一个测试

---
+++ onepore/src/Text.java
@@ -0,0 +1,15 @@
+public class Text {
+
+    public static void main(String[] args) {
+        System.out.println("this is a text page");
+        System.out.println("this is a text page");
+        System.out.println("this is a text page");
+    }
+
+
+
+
+
+
+
+}
